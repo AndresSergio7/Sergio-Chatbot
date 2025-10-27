e6ba70e8cad60bbfad0bcd38e227c5dd4347e237
AndresSergio7 <sergioandresvalleleal@gmail.com>
2025-10-27 09:33:38 -0600

    Reorder imports for clarity in app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import TextLoader, PyMuPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
+from langchain.chains import create_stuff_documents_chain
+from langchain.retrieval import create_retrieval_chain
 # Note: You must ensure all these packages (plus langchain-core) are in requirements.txt
 # --- END CORE IMPORTS ---
 
